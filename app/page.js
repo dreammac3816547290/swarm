71c67624c1c17fd934484b64744a722473bcb717
unknown <nathaniel2048@gmail.com>
2025-09-28 20:19:47 +0800

    Change canvas to 512 * 512, implement animation frame

--- app/page.js
+++ app/page.js
@@ -1,6 +1,6 @@
 "use client";
 
-import { use, useEffect, useRef, useState } from "react";
+import { useEffect, useRef, useState } from "react";
 
 export default function Home() {
   const canvasRef = useRef(null);
@@ -21,6 +21,35 @@ export default function Home() {
   }, [tool, size, color]);
 
   // download png, upload png, select from png URL
+
+  const [state, setState] = useState(null);
+  const [play, setPlay] = useState(false);
+  const [fps, setFps] = useState(24);
+  const [start, setStart] = useState(0);
+  const [frame, setFrame] = useState(0);
+
+  function callback(t) {
+    if (!play) return;
+    if ((t - start) / 1000 >= frame / fps) {
+      // visualize
+      setState(next);
+      setFrame((frame) => frame + 1); // alternative jump to frame
+    } else requestAnimationFrame(callback);
+  }
+
+  useEffect(() => {
+    if (!play) return;
+    if (!state) {
+      // initialize
+    }
+    setStart(performance.now());
+    setFrame(0); // reset on pause
+    requestAnimationFrame(callback);
+  }, [play]);
+
+  useEffect(() => {
+    requestAnimationFrame(callback);
+  }, [frame]);
 
   return (
     <div style={{ padding: 10 }} onMouseUp={deactivate}>
@@ -83,8 +112,8 @@ export default function Home() {
       <br />
       <canvas
         ref={canvasRef}
-        width={500}
-        height={500}
+        width={512}
+        height={512}
         style={{ margin: 10, border: "1px solid", cursor: "crosshair" }}
         onMouseDown={(e) => {
           const canvas = canvasRef.current;
@@ -105,9 +134,22 @@ export default function Home() {
       <br />
       <button style={{ margin: 10, padding: 5 }}>Set Start</button>
       <button style={{ margin: 10, padding: 5 }}>Set End</button>
-      <button style={{ margin: 10, padding: 5 }}>Start</button>
+      <button style={{ margin: 10, padding: 5 }} onClick={() => setPlay(!play)}>
+        {play ? "Pause" : "Start"}
+      </button>
+      <input
+        type="number"
+        style={{ width: 50 }}
+        value={fps}
+        min={0}
+        onChange={(e) => setFps(Number(e.target.value || 24))}
+      />
     </div>
   );
+}
+
+function next(state) {
+  return state;
 }
 
 // import { useRef, useState, useEffect } from "react";
